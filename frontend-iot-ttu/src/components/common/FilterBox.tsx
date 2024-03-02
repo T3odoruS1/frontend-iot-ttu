10f6bb27d37e7fb4681bf451bb354e6049ba9ece
edvildt <edgar.vildt@gmail.com>
2024-03-02 23:11:19 +0200

    ui improvements

--- frontend-iot-ttu/src/components/common/FilterBox.tsx
+++ frontend-iot-ttu/src/components/common/FilterBox.tsx
@@ -1,80 +1,66 @@
-import TopicAreaElement from "../../routes/public/news/list/TopicAreaElement";
 import {FC, useEffect, useState} from "react";
-import {ITopicAreaWithChildren} from "../../dto/topicarea/ITopicAreaWithChildren";
-import i18n from "i18next";
+import {useCollapse} from "react-collapsed";
 import {useTranslation} from "react-i18next";
 import useFetch from "../../hooks/useFetch";
 import {TopicAreaService} from "../../services/TopicAreaService";
+import TopicAreaElement from "../../routes/public/news/list/TopicAreaElement";
+import {ITopicAreaWithChildren} from "../../dto/topicarea/ITopicAreaWithChildren";
 
 interface IProps {
-    onTopicAreaChange: (newTopicArea: string | null) => void
+    onTopicAreaChange: (newTopicArea: string | null) => void;
 }
 
-const FilterBox: FC<IProps> = ({onTopicAreaChange, ...rest}) => {
-    const {t} = useTranslation();
+const FilterBox: FC<IProps> = ({onTopicAreaChange}) => {
+    const {t, i18n} = useTranslation();
     const service = new TopicAreaService();
-    const {data: topicAreas, pending: tPending} =
-        useFetch<ITopicAreaWithChildren[]>(service.getAll, [i18n.language]);
+    const {data: topicAreas, pending: tPending} = useFetch<ITopicAreaWithChildren[]>(service.getAll, [i18n.language]);
 
     const isInitiallyMobile = window.innerWidth < 768;
-
-    const [selected, setSelected] = useState(!isInitiallyMobile);
+    const [isExpanded, setExpanded] = useState(!isInitiallyMobile);
     const [mobile, setMobile] = useState(isInitiallyMobile);
 
+    const {getToggleProps, getCollapseProps} = useCollapse({isExpanded});
+
+    useEffect(() => {
+        console.log(isExpanded)
+    }, [isExpanded]);
 
     useEffect(() => {
         const handleResize = () => {
             const isMobileNow = window.innerWidth < 768;
-
             if (mobile !== isMobileNow) {
                 setMobile(isMobileNow);
-                setSelected(!isMobileNow);
+                setExpanded(!isMobileNow);
             }
         };
 
         window.addEventListener('resize', handleResize);
-
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
+        return () => window.removeEventListener('resize', handleResize);
     }, [mobile]);
 
-
     return (
-        <div className={`filter_box ${!selected ? "clickable-pointer" : ""}  ${mobile ? "mb-2" : ""}`}>
+        <div className={`collapse-card p-2 ${!isExpanded ? "clickable-pointer" : ""} ${mobile ? "mb-2" : ""}`}>
             {tPending && <p>Loading...</p>}
-
-            <div className={"filter_header w-100"}>
-                <h3 onClick={() => {
-                    setSelected(!selected)
-                }} className="w-100 header-pink unselectable m-0">
+            <div {...getToggleProps({
+                onClick: () => setExpanded((prevExpanded) => !prevExpanded),
+            })} className={"filter_header w-100"}>
+                <h3 className="w-100 header-pink unselectable m-0">
+                    <span
+                        className={isExpanded ? "expanded-collapse-arrow" : "collape-arrow"}>›
+                    </span>
                     {t("public.news.filters")}
                 </h3>
             </div>
-            {selected && <ul className={`p-0 ${selected ? "open" : ""}`}>
-                {topicAreas
-                    ?.sort((a, b) => {
-                        let nameA = a.name.toLowerCase();
-                        let nameB = b.name.toLowerCase();
+            <ul className={"p-2"} {...getCollapseProps()}>
+                {topicAreas?.map((topicArea) => (
+                    <TopicAreaElement
+                        key={topicArea.id} // Assuming each topicArea has a unique 'id'
+                        topicArea={topicArea}
+                        onTopicAreaChange={onTopicAreaChange}
+                    />
+                ))}
 
-                        if (nameA < nameB) {
-                            return -1;
-                        }
-                        if (nameA > nameB) {
-                            return 1;
-                        }
-
-                        return 0;
-                    })
-                    .map((topicArea) => {
-                        return (<TopicAreaElement
-                                topicArea={topicArea}
-                                onTopicAreaChange={onTopicAreaChange}
-                            />
-
-                        );
-                    })}
-            </ul>}
+            </ul>
         </div>
     );
 };
