70427d117b006930e16fe0239951fe41e6ac41c4
edvildt <edgar.vildt@gmail.com>
2024-04-12 15:49:29 +0300

    demo

--- frontend-iot-ttu/src/routes/admin/admin/AdminLandingPage.tsx
+++ frontend-iot-ttu/src/routes/admin/admin/AdminLandingPage.tsx
@@ -1,31 +1,14 @@
-import {IdentityService} from "../../../services/IdentityService";
-import useFetch from "../../../hooks/useFetch";
-import {IRole} from "../../../dto/identity/IRole";
-import {JwtContext} from "../../Root";
-import {useContext} from "react";
-import PageTitle from "../../../components/common/PageTitle";
+import {useEffect} from "react";
+import {useNavigate} from "react-router-dom";
 
-// interface IRoute {
-//     route: string,
-//     title: string,
-//     description: string
-// }
 
 const AdminLandingPage = () => {
-    const {jwtResponseCtx, setJwtResponseCtx} = useContext(JwtContext);
-
-    const service = new IdentityService();
-    const {data: roles, pending, error} = useFetch<IRole[]>(service.getRoles)
-
-
+   const navigate = useNavigate();
+    useEffect(() => {
+        navigate("./news")
+    }, []);
     return (
         <>
-            <PageTitle>Admin panel</PageTitle>
-            <div>
-                <h5 className={""}>Logged in as: {jwtResponseCtx?.username}</h5>
-                <h5 className={""}>With
-                    role: {roles?.find(r => r.id === jwtResponseCtx?.roleIds?.at(0))?.name}</h5>
-            </div>
         </>
     );
 };
